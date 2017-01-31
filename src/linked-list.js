f3f063d120e2a05db8b797cc5b999f786a0cb908
Natallia <nlukyaniuk@gmail.com>
2017-02-01 00:39:15 +0300

    doubly-linked-list:added chaining and code refactoring

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,15 +9,18 @@ class LinkedList {
 
     append(data) {
         var node = new Node(data);
-        if (this.length == 0) {
+
+        if (this.length === 0) {
             this._head = node;
-        }
-        else {
+        } else {
             this._tail.next = node;
             node.prev = this._tail;
         }
+
         this._tail = node;
         this.length++;
+
+        return this;
     }
 
     head() {
@@ -29,108 +32,131 @@ class LinkedList {
     }
 
     at(index) {
-        var i = 0,
-            current = this._head;
-        while (current != null && i != index) {
+        var i = 0;
+        var result = null;
+        var current = this._head;
+
+        while (current !== null && i !== index) {
             current = current.next;
             i++;
         }
-        if (current == null) {
-            return;
+
+        if (current !== null) {
+            result = current.data;
         }
-        return current.data;
+
+        return result;
     }
 
     insertAt(index, data) {
-        var node = new Node(data),
-            current = this._head,
-            i = 0;
-        while (current != null && i!= index) {
+        var node = new Node(data);
+        var current = this._head;
+        var i = 0;
+
+        while (current !== null && i !== index) {
             i++;
             current = current.next;
         }
-        if (current == null) {
-            return;
+
+        if (current !== null) {
+            var temp = current.prev;
+            current.prev = node;
+            temp.next = node;
+            node.next = current;
+            node.prev = temp;
         }
-        var temp = current.prev;
-        current.prev = node;
-        temp.next = node;
-        node.next = current;
-        node.prev = temp;
+
+        return this;
     }
 
     isEmpty() {
-        if (this.length == 0) 
-        return true;
-        return false;
+        return this.length === 0;
     }
 
     clear() {
-        this._head.data = null;
-        this._tail.data = null;
+        if (this._head) {
+            this._head.data = null;
+        }
+
+        if (this._tail) {
+            this._tail.data = null;
+        }
+
         this.length = 0;
+
+        return this;
     }
 
     deleteAt(index) {
-        var current = this._head,
-            previous = null,
-            i = 0;
-            while (current != null && i != index) {
-                i++;
-                previous = current;
-                current = current.next;
+        var current = this._head;
+        var previous = null;
+        var i = 0;
+
+        while (current !== null && i !== index) {
+            i++;
+            previous = current;
+            current = current.next;
+        }
+
+        if (previous !== null) {
+            if (current.next === null) {
+                this._tail = previous;
+            } else {
+                current.next.prev = previous;
+                current.prev.next = current.next;
             }
-            if (previous != null) {
-                if (current.next == null) {
-                    this._tail = previous;
-                }
-                else {
-                    current.next.prev = previous;
-                    current.prev.next = current.next;
-                }
+            this.length--;
+        } else {
+            if (this.length !== 0) {
+                this._head = this._head.next;
                 this.length--;
-            }
-            else {
-                if (this.length != 0) {
-                    this._head = this._head.next;
-                    this.length--;
-                    if (this.length == 0) {
-                        this._tail = null;
-                    }
-                    else {
-                        this._head.prev = null;
-                    }
+                if (this.length === 0) {
+                    this._tail = null;
+                } else {
+                    this._head.prev = null;
                 }
             }
+        }
+
+        return this;
     }
 
     reverse() {
-        var current = this._head,
-            tmp;
-        while(current) {
+        var current = this._head;
+        var tmp;
+
+        while (current) {
             if (!current.prev) {
                 this._tail = current;
             }
+
             tmp = current.next;
             current.next = current.prev;
             current.prev = tmp;
+
             if (!tmp) {
                 this._head = current;
             }
+
             current = tmp;
         }
+
+        return this;
     }
 
     indexOf(data) {
-        var current = this._head,
-            i = 0;
-        while (current != null) {
+        var current = this._head;
+        var i = 0;
+
+        while (current !== null) {
             if (current.data === data) {
                 return i;
             }
+
             i++;
             current = current.next;
         }
+
         return -1;
     }
 }
